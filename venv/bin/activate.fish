bcbe854a0109bd06bfdb7c0607a714ba64a2193d
EC2 Default User <ec2-user@ip-172-31-42-11.eu-west-3.compute.internal>
2023-04-25 22:51:15 +0000

    RDS congig

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ec2-user/environment/Lib_Management/venv"
+set -gx VIRTUAL_ENV "/home/ec2-user/environment/Library_Management_Django_Project/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
